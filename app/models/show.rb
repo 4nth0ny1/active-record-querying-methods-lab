b07e76231eb4e5b79b8ae5f0e5b630843f56e3dc
4anth0ny1 <acatullo4@gmail.com>
2021-02-24 23:12:49 -0600

    commit tree 4ba27a3bc95a64db406169361a8ad115d4458500

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,8 +8,10 @@ class Show < ActiveRecord::Base
 
     # .most_popular_show
     # returns the name of the TV show with the highest rating (FAILED - 2)
+    # THIS WORKS TOO self.find_by_sql("SELECT * FROM shows WHERE rating = 10 LIMIT 1").first
+
     def self.most_popular_show
-        self.find_by_sql("SELECT * FROM shows WHERE rating = 10 LIMIT 1").first
+        self.where("rating = ?", self.highest_rating).first
     end 
 
     # .lowest_rating
@@ -21,7 +23,7 @@ class Show < ActiveRecord::Base
     # .least_popular_show
     # returns the name of the TV show with the lowest rating (FAILED - 4)
     def self.least_popular_show
-
+        self.where("rating = ?", self.lowest_rating).first
     end
 
     # .ratings_sum
@@ -33,13 +35,13 @@ class Show < ActiveRecord::Base
     # .popular_shows
     # returns an array of all of the shows with a rating above 5 (FAILED - 6)
     def self.popular_shows
-
+        self.where("rating > 5", self.all)
     end 
 
     # .shows_by_alphabetical_order
     # returns an array of all of the shows, listed in alphabetical order (FAILED - 7)
     def self.shows_by_alphabetical_order
-
+        self.all.order("name ASC")
     end
 end
 
